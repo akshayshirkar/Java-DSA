4cd9f906adfffdf2d6ad5b66790681ec3a57d634
Akshay Shikar <akshayshirkardev@gmail.com>
2025-01-16 21:43:51 +0530

    One dimensinol Array

---
+++ Advance_DSA/Lecture_One _One Dimensinol_Array/Lecture_One_OneDimensinol_Array/maxsumsubarrayCarrayForward.java
@@ -0,0 +1,17 @@
+public class maxsumsubarrayCarrayForward {
+    public static void main(String[] args) {
+		//System.out.println("Hello World");
+		int [] arr = {1,2,3};
+		int max_sum = 0;
+		for(int i =0;i<arr.length;i++){
+		    int sum = 0;
+		    for(int j = i;j<arr.length;j++){
+		        sum += arr[j]; 
+		    }
+		    if(sum  > max_sum){
+		        max_sum = sum;
+		    }
+		}
+		System.out.println("Max sum is "+max_sum);
+	}
+}
